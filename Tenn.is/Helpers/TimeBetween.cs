2edfeb74d80aac90058a47ccd2804778b7b6d336
Marius Egede Knudsen <mak004@edu.zealand.dk>
2024-04-22 10:54:14 +0200

    TimeBetween

--- Tenn.is/Helpers/TimeBetween.cs
+++ Tenn.is/Helpers/TimeBetween.cs
@@ -2,5 +2,23 @@
 {
     public class TimeBetween
     {
+
+        public TimeBetween(DateTime startTime, DateTime endTime)
+        {
+            if (EndTime <= startTime)
+            {
+                throw new ArgumentException("Start Time is larger than End Time");
+            }
+            StartTime = startTime;
+            EndTime = endTime;
+        }
+        public DateTime StartTime { get; set; }
+
+        public DateTime EndTime { get; set; }
+
+        public TimeSpan TimeSpan { get {
+                return EndTime - StartTime;
+            } }
+
     }
 }
