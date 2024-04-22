e746bc297fc77cd95c704a0a6de2f16e96148045
Stéphane Karlsen <stk002@edu.zealand.dk>
2024-04-22 11:31:39 +0200

    InterfaceTest

---
+++ TennisTest/ILaneServiceTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TennisTest
+{
+    [TestClass]
+    public class ILaneServiceTest
+    {
+
+        void TestSetUp()
+        {
+
+        }
+
+        [TestMethod]
+        public void GetAllLanes()
+        {
+            //using (SqlConnection connection = new SqlConnection(connectionString))
+
+
+
+        }
+
+
+
+        [TestMethod]
+        public void GetLaneByNumber()
+        {
+
+        }
+
+
+
+        [TestMethod]
+        public void CreateLaneTestAcceptableValues()
+        {
+
+        }
+
+
+
+        [TestMethod]
+        public void CreateLaneTestUnacceptableValues()
+        {
+
+        }
+
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+
+        }
+
+    }
+}
