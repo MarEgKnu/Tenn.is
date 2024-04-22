8177b89d1285610e2dc39d3834fb67b286068124
Marius Egede Knudsen <mak004@edu.zealand.dk>
2024-04-22 10:37:07 +0200

    Moar interfaces

---
+++ Tenn.is/Interfaces/IEventBookingService.cs
@@ -0,0 +1,15 @@
+﻿namespace Tennis.Interfaces
+{
+    public interface IEventBookingService
+    {
+        bool CreateEventBooking(EventBooking eventBooking);
+
+        bool DeleteEventBooking(int id);
+
+        bool EditEventBooking(EventBooking eventBooking, int id);
+
+        List<EventBooking> GetAllEventBookings();
+
+        EventBooking GetEventBookingById(int id);
+    }
+}
