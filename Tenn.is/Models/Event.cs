0dc911362c95a819d6c45c46c7808eb695b8bffd
Marius Egede Knudsen <mak004@edu.zealand.dk>
2024-04-23 11:07:11 +0200

    basic unit tests for event CRUD

--- Tenn.is/Models/Event.cs
+++ Tenn.is/Models/Event.cs
@@ -1,6 +1,28 @@
-﻿namespace Tennis.Models
+﻿using Tennis.Helpers;
+
+namespace Tennis.Models
 {
     public class Event
     {
+        public Event(int eventId, string title, int cancellationThresholdMinutes, string description, TimeBetween eventTime)
+        {
+            EventID = eventId;
+            Title = title;
+            CancellationThresholdMinutes = cancellationThresholdMinutes;
+            Description = description;
+            EventTime = eventTime;
+            Cancelled = false;
+        }
+        public int EventID { get; set; }
+
+        public string Title { get; set; }
+
+        public bool Cancelled { get; set; }
+
+        public int CancellationThresholdMinutes { get; set; }
+
+        public string Description { get; set; }
+
+        public TimeBetween EventTime {  get; set; }
     }
 }
--- TennisTest/IEventServiceTest.cs
+++ TennisTest/IEventServiceTest.cs
@@ -1,23 +1,189 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tennis.Exceptions;
+using Tennis.Helpers;
+using Tennis.Interfaces;
+using Tennis.Models;
+using Tennis.Services;
 
 namespace TennisTest
 {
     [TestClass]
     public class IEventServiceTest
     {
+        private IEventService eventService;
 
         void TestSetUp()
         {
+            eventService = new EventService(true);
+            using (SqlConnection conn = new SqlConnection(Secret.ConnectionStringTest))
+            {
+                SqlCommand cmd = new SqlCommand("DELETE FROM Events", conn);
 
+                cmd.ExecuteNonQuery();
+            }
         }
 
         [TestMethod]
-        public void TestMethod1()
+        public void CreateEventTest_Sucess()
         {
+            TestSetUp();
+            List<Event> oldEvents = eventService.GetAllEvents();
+            Event newEvent = new Event(1, "epic event", 0, "big event",new TimeBetween(new DateTime(2030,12,5), new DateTime(2030, 12, 6)));
+            bool sucess = eventService.CreateEvent(newEvent);
+            List<Event> newEvents = eventService.GetAllEvents();
+            Assert.AreEqual(oldEvents.Count + 1, newEvents.Count);
+            Assert.IsTrue(sucess);
+            Assert.AreEqual("epic event", newEvents[0].Title);
+            Assert.AreEqual("big event", newEvents[0].Description);
         }
+        [ExpectedException(typeof(ArgumentException))]
+        [TestMethod]
+        public void CreateEventTest_Fail_StartTimeBiggerThanEndTime()
+        {
+            TestSetUp();
+            Event newEvent = new Event(1, "epic event", 0, "big event", new TimeBetween(new DateTime(2030, 12, 6), new DateTime(2030, 12, 5)));
+            bool sucess = eventService.CreateEvent(newEvent);
+        }
+        [ExpectedException(typeof(InvalidTimeException))]
+        [TestMethod]
+        public void CreateEventTest_Fail_EventIsInThePast()
+        {
+            TestSetUp();
+            Event newEvent = new Event(1, "epic event", 0, "big event", new TimeBetween(new DateTime(2022, 12, 6), new DateTime(2022, 12, 7)));
+            bool sucess = eventService.CreateEvent(newEvent);
+        }
+
+        [TestMethod]
+        public void GetAllEvents_0Events()
+        {
+            TestSetUp();
+            List<Event> events = eventService.GetAllEvents();
+            Assert.AreEqual(0, events.Count);
+        }
+
+        [TestMethod]
+        public void GetAllEvents_3Events()
+        {
+            TestSetUp();
+            List<Event> oldEvents = eventService.GetAllEvents();
+            Event newEvent = new Event(1, "epic event", 0, "big event", new TimeBetween(new DateTime(2030, 12, 6), new DateTime(2030, 12, 7)));
+            Event newEvent2 = new Event(2, "epic event", 0, "big event", new TimeBetween(new DateTime(2030, 12, 6), new DateTime(2030, 12, 7)));
+            Event newEvent3 = new Event(3, "epic event", 0, "big event", new TimeBetween(new DateTime(2030, 12, 6), new DateTime(2030, 12, 7)));
+            eventService.CreateEvent(newEvent);
+            eventService.CreateEvent(newEvent2);
+            eventService.CreateEvent(newEvent3);
+            List<Event> newEvents = eventService.GetAllEvents();
+            Assert.AreEqual(oldEvents.Count + 3, newEvents.Count);
+        }
+
+        [TestMethod]
+        public void DeleteEventTest_Sucess()
+        {
+            TestSetUp();
+            Event newEvent = new Event(1, "epic event", 0, "big event", new TimeBetween(new DateTime(2030, 12, 6), new DateTime(2030, 12, 7)));
+            eventService.CreateEvent(newEvent);
+            int oldEventsCount = eventService.GetAllEvents().Count;
+            newEvent = eventService.GetAllEvents()[0];
+            bool sucess = eventService.DeleteEvent(newEvent.EventID);
+            int newEventsCount = eventService.GetAllEvents().Count;
+            Assert.AreEqual(oldEventsCount-1, newEventsCount);
+            Assert.IsTrue(sucess);
+        }
+
+        [TestMethod]
+        public void DeleteEventTest_Fail_EventNotExist()
+        {
+            TestSetUp();
+            Event newEvent = new Event(1, "epic event", 0, "big event", new TimeBetween(new DateTime(2030, 12, 6), new DateTime(2030, 12, 7)));
+            eventService.CreateEvent(newEvent);
+            int oldEventsCount = eventService.GetAllEvents().Count;
+            bool sucess = eventService.DeleteEvent(999999);
+            int newEventsCount = eventService.GetAllEvents().Count;
+            Assert.AreEqual(oldEventsCount, newEventsCount);
+            Assert.IsFalse(sucess);
+        }
+        [TestMethod]
+        public void GetEventByIdTest_Sucess()
+        {
+            TestSetUp();
+            Event newEvent = new Event(1, "epic evente", 0, "big evente", new TimeBetween(new DateTime(2030, 12, 6), new DateTime(2030, 12, 7)));
+            eventService.CreateEvent(newEvent);
+            List<Event> events = eventService.GetAllEvents();
+            Event grabbedEvent = eventService.GetEventByNumber(events[0].EventID);
+            Assert.AreEqual(newEvent.Title, grabbedEvent.Title);
+            Assert.AreEqual(newEvent.Description, grabbedEvent.Description);
+        }
+        [TestMethod]
+        public void GetEventByIdTest_Fail_NoEventFound()
+        {
+            TestSetUp();
+            Event newEvent = new Event(1, "epic evente", 0, "big evente", new TimeBetween(new DateTime(2030, 12, 6), new DateTime(2030, 12, 7)));
+            eventService.CreateEvent(newEvent);
+            List<Event> events = eventService.GetAllEvents();
+            Event grabbedEvent = eventService.GetEventByNumber(999999);
+            Assert.IsNull(grabbedEvent);
+         
+        }
+
+
+
+
+
+
+        [TestMethod]
+        public void EditEventTest_Sucess()
+        {
+            TestSetUp();
+            Event newEvent = new Event(1, "epic event", 0, "big event", new TimeBetween(new DateTime(2030, 12, 6), new DateTime(2030, 12, 7)));
+            Event editedEvent = new Event(2, "big event", 0, "epic event", new TimeBetween(new DateTime(2030, 12, 6), new DateTime(2030, 12, 7)));
+            eventService.CreateEvent(newEvent);
+            List<Event> events = eventService.GetAllEvents();
+            bool sucess = eventService.EditEvent(editedEvent, events[0].EventID);
+            Event afterEdit = eventService.GetEventByNumber(events[0].EventID);
+            Assert.AreEqual("big event", afterEdit.Title);
+            Assert.AreEqual("epic event", afterEdit.Description);
+            Assert.AreEqual(events[0].EventID, afterEdit.EventID);
+            Assert.IsTrue(sucess);
+        }
+
+
+        [TestMethod]
+        public void EditEventTest_Sucess_EventIDCantBeChanged()
+        {
+            TestSetUp();
+            Event newEvent = new Event(1, "epic event", 0, "big event", new TimeBetween(new DateTime(2030, 12, 6), new DateTime(2030, 12, 7)));
+            Event editedEvent = new Event(2, "epic event", 0, "big event", new TimeBetween(new DateTime(2030, 12, 6), new DateTime(2030, 12, 7)));
+            eventService.CreateEvent(newEvent);
+            List<Event> events = eventService.GetAllEvents();
+            bool sucess = eventService.EditEvent(editedEvent, events[0].EventID);
+            Event afterEdit = eventService.GetEventByNumber(events[0].EventID);
+            Assert.AreEqual(newEvent.Title, afterEdit.Title);
+            Assert.AreEqual(newEvent.Description, afterEdit.Description);
+            Assert.AreEqual(events[0].EventID, afterEdit.EventID);
+            Assert.IsTrue(sucess);
+        }
+
+
+        [ExpectedException(typeof(InvalidTimeException))]
+        [TestMethod]
+        public void EditEventTest_Fail_ChangeTimeToPast()
+        {
+            TestSetUp();
+            Event newEvent = new Event(1, "epic event", 0, "big event", new TimeBetween(new DateTime(2030, 12, 6), new DateTime(2030, 12, 7)));
+            Event editedEvent = new Event(2, "epic event", 0, "big event", new TimeBetween(new DateTime(2022, 12, 6), new DateTime(2022, 12, 7)));
+            eventService.CreateEvent(newEvent);
+            List<Event> events = eventService.GetAllEvents();
+            bool sucess = eventService.EditEvent(editedEvent, events[0].EventID);
+        }
+
+
+
+
+
     }
 }
