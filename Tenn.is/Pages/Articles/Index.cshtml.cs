de53862d5d0894e107631c4f98cd7ad59b9bf53d
Molle <Molle@MOLLESLAPTOP>
2024-04-30 10:52:21 +0200

    article start på user-afhængige ting

--- Tenn.is/Pages/Articles/Index.cshtml.cs
+++ Tenn.is/Pages/Articles/Index.cshtml.cs
@@ -8,17 +8,28 @@ namespace Tennis.Pages.Articles
 {
     public class IndexModel : PageModel
     {
+        public bool IsLoggedIn = false;
+        public bool IsAdmin = false;
+
         public List<Article> AllArticles { get; set; }
+        public UserService _userService {  get; set; }
 
         private IArticleService _articleService;
-        public IndexModel(IArticleService articleService)
+        public IndexModel(IArticleService articleService, UserService userService)
         {
             _articleService = new ArticleService(true);
+            _userService = userService;
         }
 
         public void OnGet()
         {
             AllArticles = _articleService.GetAllArticles();
+
+            if (_userService.VerifyUser(HttpContext.Session.GetString("Username"), HttpContext.Session.GetString("Password")) != null)
+            {
+                IsLoggedIn = true;
+                //if (_userService.AdminVerify() { }
+            } 
         }
     }
 }
