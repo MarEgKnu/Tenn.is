841af800118a3c7a3befaa231036b90642399de0
Marius Egede Knudsen <mak004@edu.zealand.dk>
2024-05-23 11:06:47 +0200

    Cancelling lane booking page works for both traininglanebookings and userlanebookings

--- Tenn.is/Pages/Lanes/BookingCancel.cshtml.cs
+++ Tenn.is/Pages/Lanes/BookingCancel.cshtml.cs
@@ -15,6 +15,12 @@ namespace Tennis.Pages.Lanes
         public User Booker {  get; set; }
         public User Mate { get; set; }
         public UserLaneBooking CurrentBooking { get; set; }
+
+        public TrainingTeam TrainingTeam { get; set; }
+
+        bool IsTraining { get; set; }
+
+        public TrainingLaneBooking CurrentBookingTraining { get; set; }
         public string Message { get; set; }
 
         public BookingCancelModel(ILaneBookingService bookingService, IUserService userService)
@@ -24,19 +30,38 @@ namespace Tennis.Pages.Lanes
         }
         public IActionResult OnGet(int bookingid)
         {
-            if (HttpContext.Session.GetString("Username") == null)
-            {
-                return RedirectToPage("/Users/Login", "Redirect", new { message = "Du skal være logget ind for at booke en bane" });
-            }
+            
             try
             {
-                CurrentBooking = _bookingService.GetUserLaneBookingById(bookingid);
+                
                 CurrentUser = _userService.VerifyUser(HttpContext.Session.GetString("Username"), HttpContext.Session.GetString("Password"));
-                Booker = _userService.GetUserById(CurrentBooking.UserID);
-                Mate = _userService.GetUserById(CurrentBooking.MateID);
-                if (!(CurrentUser.Administrator) && (CurrentBooking.UserID != CurrentUser.UserId && CurrentBooking.MateID != CurrentUser.UserId))
+                CurrentBooking = _bookingService.GetUserLaneBookingById(bookingid);
+                if (CurrentBooking == null)
                 {
-                    return RedirectToPage("/Users/Login", "Redirect", new { message = "Du kan ikke aflyse bookinger bookinger du ikke selv er en del af" });
+                    IsTraining = true;
+                    CurrentBookingTraining = _bookingService.GetTrainingLaneBookingById(bookingid);
+                    if (CurrentBookingTraining == null)
+                    {
+                        ViewData["ErrorMessage"] = "Fejl, bookingen eksistere ikke";
+                        return Page();
+                    }
+                    TrainingTeam = CurrentBookingTraining.trainingTeam;
+                    if (!CurrentUser.Administrator && !TrainingTeam.IsTrainer(CurrentUser))
+                    {
+                        return RedirectToPage("/Users/Login", "Redirect", new { message = "Du skal være logget ind som administrator eller træner for at kunne aflyse denne booking" });
+                    }                 
+                   
+                }
+                else
+                {
+                    
+                    IsTraining = false;                   
+                    if (!(CurrentUser.Administrator) && (CurrentBooking.UserID != CurrentUser.UserId && CurrentBooking.MateID != CurrentUser.UserId))
+                    {
+                        return RedirectToPage("/Users/Login", "Redirect", new { message = "Du kan ikke aflyse bookinger bookinger du ikke selv er en del af" });
+                    }
+                    Booker = _userService.GetUserById(CurrentBooking.UserID);
+                    Mate = _userService.GetUserById(CurrentBooking.MateID);
                 }
             }
             catch (SqlException sqlExp)
@@ -60,11 +85,7 @@ namespace Tennis.Pages.Lanes
                 if (!_bookingService.CancelLaneBonking(bookingid))
                 {
                     Message = "Bookingen kunne ikke aflyses. Prøv at navigere til denne side igen";
-                    CurrentBooking = _bookingService.GetUserLaneBookingById(bookingid);
-                    CurrentUser = _userService.VerifyUser(HttpContext.Session.GetString("Username"), HttpContext.Session.GetString("Password"));
-                    Booker = _userService.GetUserById(CurrentBooking.UserID);
-                    Mate = _userService.GetUserById(CurrentBooking.MateID);
-                    return Page();
+                    return OnGet(bookingid);
                 }
                 else
                 {
